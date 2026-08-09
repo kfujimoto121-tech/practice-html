9637c857c1785707bd00d3ac612815394a697ca4
kfujimoto121 <k.fujimoto12.1@gmail.com>
2026-08-09 18:56:20 +0900

    文字の表示，非表示

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 //htmlからボタンと箱の要素を取得する
 const button = document.getElementById("colorButton");
-const box = document.getElementById("myBox");
+const text = document.getElementById("myText");
 
-//現在の箱の状態を記憶しておくフラグ変数(boolean)をつくる
-let isTomato = false; //最初はトマト色ではないのでfalse
+//現在の文字の状態を記憶しておくフラグ変数(boolean)をつくる
+let isVisible = true; //最初は表示されているのでtrue
 
 //ボタンをクリックしたときの処理
 button.addEventListener("click", function(){
-    if(isTomato === false){
-        box.style.backgroundColor = "tomato";
-        isTomato = true; //箱の色をトマト色にする
+    if(isVisible === false){
+        text.style.display = "block";
+        
+        isVisible = true; //文字を表示にする
+
+        button.textContent = "文字を非表示にする";
     }else{
-        box.style.backgroundColor = "lightblue";
-        isTomato = false; //箱の色をライトブルーにする
+        text.style.display = "none";
+        isVisible = false; //文字を非表示にする
+        button.textContent = "文字を表示する";
     }
 });
